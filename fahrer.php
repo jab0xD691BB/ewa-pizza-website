e322b9a1ce777bb619509f252b31d53dc44c27c4
joseph.acosta <joseph.acosta@stud.h-da.de>
2019-10-27 23:30:56 +0100

    validate, lint

--- fahrer.php
+++ fahrer.php
@@ -13,23 +13,25 @@
     <form action="https://echo.fbi.h-da.de/" method="POST" id="test1">
         <div id="schulzId">
         <p>Schulz, Kasinostr. 5 13,50€</p>
+        <p>Margherita, Salami, Tonno</p>
         <span>fertig</span>
         <span>unterwegs</span>
         <span>geliefert</span>
         <br>        
-        <span><input type="radio" name="status" value="fertig"></span>
+        <span><input type="radio" name="status" value="fertig" checked></span>
         <span><input type="radio" name="status" value="unterwegs"></span>
         <span><input type="radio" name="status" value="geliefert"></span>
         </div>
 
         <div id="muellerId">
         <p>Müller, Rheinstr. 11 10,00€</p>
+        <p>Salami, Prosciutto</p>
         <span>fertig</span>
         <span>unterwegs</span>
         <span>geliefert</span>
         <br>        
         <span><input type="radio" name="status1" value="fertig"></span>
-        <span><input type="radio" name="status1" value="unterwegs"></span>
+        <span><input type="radio" name="status1" value="unterwegs" checked></span>
         <span><input type="radio" name="status1" value="geliefert"></span>
         </div>
         </form>
